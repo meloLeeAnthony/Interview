3540f704c34bfa801b5d26d3d451cab7c3ca157a
li.chunlong <lcl253137@alibaba-inc.com>
2020-11-30 19:11:47 +0800

    值传递 & 引用传递

--- src/main/java/com/neusoft/transmit/PassTest.java
+++ src/main/java/com/neusoft/transmit/PassTest.java
@@ -7,22 +7,28 @@ import org.apache.commons.lang3.ArrayUtils;
  *
  * @company 富仁集团
  */
-public class Test1 {
+public class PassTest {
 
     private String str = new String("abcdefg");
 
     private char[] ch = {'1', '2', '3'};
 
     public static void main(String[] args) {
-        Test1 test = new Test1();
+        PassTest test = new PassTest();
 
         test.change(test.str, test.ch);
-
         System.out.println(test.str + " and " + ArrayUtils.toString(test.ch));
+
+        test.referenceChange(test.ch);
+        System.out.println(ArrayUtils.toString(test.ch));
     }
 
     private void change(String str, char[] ch) {
         str = "uvwxyz";
         ch[0] = '9';
+    }
+
+    private void referenceChange(char[] ch) {
+        ch = new char[]{'9'};
     }
 }
