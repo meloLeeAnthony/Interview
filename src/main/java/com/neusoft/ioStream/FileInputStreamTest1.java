4469e08281b66387bca53a984dec49f9ac64b43c
li.chunlong <li.chun158@gmail.com>
2021-01-15 22:30:53 +0800

    文件IO修改

--- src/main/java/com/neusoft/ioStream/FileInputStreamTest1.java
+++ src/main/java/com/neusoft/ioStream/FileInputStreamTest1.java
@@ -9,19 +9,22 @@ public class FileInputStreamTest1 {
     public static void main(String[] args) throws IOException {
         String str = FileUtils.readFileToString(new File("E:\\workspace_Idea\\Interview\\src\\main\\java\\readme.txt"));
         System.out.println(str);
-        // StringBuilder sb = new StringBuilder();
-        //
-        // try (InputStream is = new FileInputStream("E:\\workspace_Idea\\Interview\\src\\main\\java\\readme.txt")) {
-        //     byte[] buffer = new byte[1024];
-        //     int length = 0;
-        //     while ((length = is.read(buffer)) != -1) {
-        //         sb.append(new String(buffer, 0, length));
-        //     }
-        //     System.out.println(sb.toString());
-        // } catch (FileNotFoundException e) {
-        //     e.printStackTrace();
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
+
+        basicIO();
+    }
+
+    public static void basicIO() {
+        StringBuilder sb = new StringBuilder();
+
+        try (InputStream is = new FileInputStream("E:\\workspace_Idea\\Interview\\src\\main\\java\\readme.txt")) {
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = is.read(buffer)) != -1) {
+                sb.append(new String(buffer, 0, length));
+            }
+            System.out.println(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
